b061a130363683316d43693ebfd57b82ad3bdf80
Matthew Weitzel <mweitzel@gmail.com>
2024-02-09 14:45:38 -0700

    support multiple repos/projects for secret manager

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 function escelate_privileges() {
+  test -n "$APPLICATION_NAME"
   test -n "$SECRETS_UNAME"
   test -n "$SECRETS_PASS"
   test -n "$SECRETS_LOCATION"
@@ -12,11 +13,13 @@ function escelate_privileges() {
   local filenames=(id_ed25519 id_ed25519.pub .secrets.env)
   for filename in "${filenames[@]}"
   do
-    echo curl https://"$SECRETS_UNAME":"$SECRETS_PASS"@"$SECRETS_LOCATION"/"$filename" --cacert secrets.crt > "$filename"
+    curl https://"$SECRETS_UNAME":"$SECRETS_PASS"@"$SECRETS_LOCATION"/"$APPLICATION_NAME"/"$filename" --cacert secrets.crt > "$filename"
   done
 
   cp id_ed25519 ~/.ssh/id_ed25519
   cp id_ed25519.pub ~/.ssh/id_ed25519.pub
+  chmod  400 ~/.ssh/id_ed25519
+  chmod  400 ~/.ssh/id_ed25519.pub
   source .secrets.env
 }
 
@@ -43,7 +46,9 @@ exit 0
 }
 
 function clone() {
-  git clone "$MAIN_REPO" repo
+  # https://serverfault.com/questions/447028/non-interactive-git-clone-ssh-fingerprint-prompt
+  ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
+  git clone "$APPLICATION_REPO" repo
   cd ./repo
   exec ./init
 }
